f116f806e0e9dbe55949ff517ebfea0205d31581
Will-Low <moose.nugget.music@gmail.com>
2019-09-20 22:41:20 -0400

    Beginning to add parsing

--- molten/src/lib.rs
+++ molten/src/lib.rs
@@ -4,19 +4,39 @@ extern crate proc_macro;
 extern crate quote;
 
 use proc_macro::TokenStream;
+use syn::FnArg::Typed;
 use syn::Item;
-use syn::spanned::Spanned;
+use syn::Pat::Ident;
+
+struct FnInfo<'a, T: 'a> {
+    fn_name: String,
+    args: Vec<Arg<'a, T>>
+}
+
+struct Arg<'a, T: 'a> {
+    arg_name: &'a str,
+    values: Vec<T>
+}
 
 #[proc_macro_attribute]
 pub fn molten(_metadata: TokenStream, input: TokenStream) -> TokenStream {
     let item: syn::Item = syn::parse(input).expect("failed to parse input");
-
     match item {
         Item::Fn(ref fn_item) => {
-            println!("{:?}", fn_item); 
+            let fn_struct = FnInfo {
+                                fn_name: fn_item.sig.ident.to_string(),
+                                args: fn_item.sig.inputs.iter().map(|input| 
+                                    if let Typed(typed) = input {
+                                        let pat = typed.pat;
+                                        if let Ident(pat_ident) = *pat {
+                                            let arg_name = pat_ident.ident.to_string();
+                                        }
+                                    }).collect()
+                                };
+            println!("{}", fn_item.sig.ident.to_string()); 
         },
         _ => {
-            item.span().unstable(); 
+            eprintln!("Type not currently supported.");
         }
     }
 

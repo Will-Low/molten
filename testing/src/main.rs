0842533ea8e17538cee79d5a15c0b2aa3a8f80de
Will-Low <moose.nugget.music@gmail.com>
2019-09-27 15:34:33 -0400

    Moving to macro_use type macro

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,18 +1,16 @@
+extern crate proc_macro2;
 extern crate syn;
 
 use std::fs::File;
 use std::io::prelude::*;
-use syn::*;
-use syn::parse::*;
-
 
 macro_rules! molten {
     ($x:expr) => {
-        let mut file = File::open($x).unwrap();
+        let mut file = File::open($x).expect("Unable to open file");
         let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-        let stream = syn::parse_str::<TypeInfer>(&contents);
-        println!("{:?}", syn::parse::Parse::parse(stream.unwrap().underscore_token));
+        file.read_to_string(&mut contents).expect("Unable to read file");
+        let file_text = syn::parse_file(&contents).expect("Unable to parse file");
+        println!("{:#?}", file_text.items);
     }
 }
 
@@ -24,8 +22,6 @@ fn sum(value1: i64, value2: i64) -> i64 {
 fn subtract(value1: i64, value2: i64) -> i64 {
     value1 - value2
 }
-
-
 
 fn main() {
 
